df130d20ff081f42b42dac8837a06a0b8ae9a56b
selen-nium <153980794+selen-nium@users.noreply.github.com>
2025-04-02 09:25:48 +0800

    Initial Commit

---
+++ HydrAID(iosver)Tests/HydrAID_iosver_Tests.swift
@@ -0,0 +1,16 @@
+//
+//  HydrAID_iosver_Tests.swift
+//  HydrAID(iosver)Tests
+//
+//  Created by selen on 2025.04.02.
+//
+
+import Testing
+
+struct HydrAID_iosver_Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ HydrAID(iosver)UITests/HydrAID_iosver_UITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  HydrAID_iosver_UITestsLaunchTests.swift
+//  HydrAID(iosver)UITests
+//
+//  Created by selen on 2025.04.02.
+//
+
+import XCTest
+
+final class HydrAID_iosver_UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
